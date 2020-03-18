6690bb5ed02bc89bc05bcd36944763678ce5f270
Gellory <43223019+Gellory@users.noreply.github.com>
2020-03-18 18:25:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const prefix = '+'
     client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setStatus("dnd");
-    client.user.setActivity("lost angel ",{type: 'WATCHING'});
+    client.user.setActivity("The Attack of Giants",{type: 'WATCHING'});
     
 });
 /*
